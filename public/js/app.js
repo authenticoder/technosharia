0c5ef6334cace38dc3caf3b75b9eae800a14716d
authenticoder <authenticoderio@gmail.com>
2021-08-30 14:09:58 +0300

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,36 @@
+const contactForm = document.querySelector('.contact-form');
+
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let subject = document.getElementById('subject');
+let message = document.getElementById('message');
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let formData = {
+        name: name.value,
+        email: email.value,
+        subject: subject.value,
+        message: message.value
+    };
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function() {
+        if(xhr.responseText == 'Success') {
+            console.log(xhr.responseText);
+            alert('Email sent');
+            name.value = '';
+            email.value = '';
+            subject.value = '';
+            message.value = '';
+        } else {
+            console.log(xhr);
+            alert("something went absolutely wrong!!!");
+        }
+    };
+
+    xhr.send(JSON.stringify(formData));
+});
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const nodemailer = require('nodemailer');
+
+const PORT = process.env.PORT || 8080;
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/contactform.html');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        secure: true,
+        auth: {
+            user: 'authenticoderio@gmail.com',
+            pass: 'Dicaprioliamsy646913',
+        }
+    });
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'authenticoderio@gmail.com',
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('Success');
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
